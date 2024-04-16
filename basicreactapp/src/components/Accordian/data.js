9ec0a0fe1d8def513ed56427cc381ab6471bedbe
Karthik Revuri <113636236+ThatLowIQguy@users.noreply.github.com>
2024-04-15 22:11:08 -0400

    Sample commit
    this is a sample.

---
+++ basicreactapp/src/components/Accordian/data.js
@@ -0,0 +1,13 @@
+const data =[
+    {
+        id : "1",
+        question : "What is React?",
+        answer : "React is a JS library that is used to build Dynamic UI interfaces."
+    },
+    {
+        id : "2",
+        question : "What is prop drilling?",
+        answer : "React shares data between components. When data is communicated between multiple levels of hierarchy it is prop-drlling."
+    }
+]
+export default data;
